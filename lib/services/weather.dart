c7341b5e4fb3074dcb3407bc5b459252352c5a51
Raahat Wani <raahatwani@gmail.com>
2023-12-25 11:11:54 +0530

    modified:   lib/services/weather.dart

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,7 +1,7 @@
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 
-
+const apiKey = '';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
